f4044511ec26a818af5eb8f7fe65dd7df31041ef
KristijanJ <kristijan.j92@gmail.com>
2022-08-06 16:59:14 +0200

    async await

--- notester-server/models/notes/index.js
+++ notester-server/models/notes/index.js
@@ -10,37 +10,35 @@ const Note = mongoose.model(
     'notes'
 );
 
-const getNotes = (req, res) => {
-    Note.find()
-        .exec()
-        .then((data) => {
-            res.status(200).send(data);
-        })
-        .catch((err) => {
-            console.log(err);
-            res.status(500).send('internal server error');
-        });
+const getNotes = async (req, res) => {
+    try {
+        const notes = await Note.find().exec();
+        res.status(200).send(notes);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('internal server error');
+    }
 };
 
-const getNote = (req, res) => {
-    Note.findOne({ _id: req.params.id }, (err, data) => {
-        if (err) {
-            console.log(err);
-            res.status(500).send("internal server error");
-        }
-        res.status(200).send(data);
-    })
+const getNote = async (req, res) => {
+    try {
+        const note = await Note.findOne({ _id: req.params.id }).exec();
+        res.status(200).send(note);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("internal server error");
+    }
 };
 
-const addNote = (req, res) => {
-    let note = new Note({ ...req.body });
-    note.save((err) => {
-        if (err) {
-            console.log(err);
-            res.status(500).send('internal server error');
-        }
+const addNote = async (req, res) => {
+    try {
+        const note = new Note({ ...req.body });
+        await note.save();
         res.status(201).send('created');
-    });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('internal server error');
+    }
 };
 
 module.exports = {
